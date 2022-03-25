16f1da2ff5eb95084f3ca150b17e18b20797a5be
LOW-GEEK <vincent.gladone@colombbus.org>
2022-03-25 15:00:16 +0100

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,22 +1,30 @@
 <!-- Footer-->
-<footer class="py-5 text-color=white">
+<footer class="py-5 text-white">
     <div class="container">
-        
-        <ul class=" text-color=white">
-            <li>Qui somme nous?</li>
-            <li>CGU</li>
-            <li>CGV</li>
-            <li>Mentions légales</li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-        <div>
-            <i class="bi bi-instagram"></i>
-            <i class="bi bi-facebook"></i>
-        </div>
+      <div class="row">
+           <div class="col-6 col-lg-2 mb-3">
+                <h5> A PROPOS </h5>
+                <ul class="list-unstyled ">
+                    <li>Qui somme nous?</li>
+                    <li>CGU</li>
+                    <li>Politique de confidentialité</li>
+                    <li>Mentions légales</li>
+                </ul>
+           </div>
+           <div class="col-6 col-lg-2 mb-3 ">
+                <h5> AIDE </h5>
+                <ul class="list-unstyled text-white"> 
+                    <li><a class="text-white" href="contact.php">Nous contacter</a></li>
+                    <li>FAQ</li>
+                </ul>
+           </div>
+            <div class="col-6 col-lg-2 mb-3">
+                <i class="bi bi-instagram"></i>
+                <i class="bi bi-facebook"></i>
+            </div>
+      </div>
     </div>
     
-    <div class="container"><p class="m-0 text-white">Copyright &copy; 400 Coups de Main 2022</p></div>
+    <div class="container"><p class="m-0 text-center text-white">Copyright &copy; 400 Coups de Main 2022</p></div>
         
 </footer>
