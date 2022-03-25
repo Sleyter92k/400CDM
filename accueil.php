d5862fa04eb7c2e9bb48a631d03c086d8a3600ef
Sleyter Monteiro <sleyter.monteiro@colombbus.org>
2022-03-25 11:42:32 +0100

    nav

--- accueil.php
+++ accueil.php
@@ -11,7 +11,7 @@
         <!-- Favicon-->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;600&display=swap" rel="stylesheet">  
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;600&display=swap" rel="stylesheet">  
         <!-- Bootstrap icons-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
