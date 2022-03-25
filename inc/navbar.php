53abef5659c3da4df87c20b2556531664f490893
Sleyter Monteiro <sleyter.monteiro@colombbus.org>
2022-03-25 11:53:38 +0100

    nav

--- inc/navbar.php
+++ inc/navbar.php
@@ -2,22 +2,24 @@
 <nav id="navbar" class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container px-4 px-lg-5">
                 <a class="navbar-brand" href="accueil.php">Logo</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navcontent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                         <li class="nav-item"><a class="nav-link active" aria-current="page" href="accueil.php">Accueil</a></li>
                         <li class="nav-item"><a class="nav-link" href="recherche.php">Recherche</a></li>
                         <li class="nav-item"><a class="nav-link" href="annonce.php">Déposer une annonce</a></li>
                         <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
                     </ul>
+                    <div class="col-md-4">
+                        <div class="row">
+                        <button class="btn btn-outline-info" type="submit"><a href="connexion.php">Connexion</a></button>
+                        <button class="btn btn-outline-info" type="submit"><a href="inscription.php">Inscription</a></button>
+                        </div>
+                    </div>
                         
-                        <button class="btn btn-outline-dark" type="submit">
-                            Connexion
-                        </button>
+                        
 
-                        <button class="btn btn-outline-dark" type="submit">
-                            Inscription
-                        </button>
+
 
                 </div>
             </div>
