cfb705fa769e48855b4814de1f3131fedb10c273
Sleyter Monteiro <sleyter.monteiro@colombbus.org>
2022-03-25 15:15:43 +0100

    form

--- recherche.php
+++ recherche.php
@@ -19,10 +19,88 @@
     <body>
         <?php require_once 'inc/navbar.php'; ?>
         
-        <header class="py-5">
+        <div class="container px-5 my-5">
+    <form id="contactForm" data-sb-form-api-token="API_TOKEN">
+        <div class="form-floating mb-3">
+            <select class="form-select" id="offreEtDemandes" aria-label="Offre et Demandes">
+                <option value="1">1</option>
+                <option value="2">2</option>
+            </select>
+            <label for="offreEtDemandes">Offre et Demandes</label>
+        </div>
+        <div class="form-floating mb-3">
+            <select class="form-select" id="donsPretsServicesBiens" aria-label="Dons / Prêts / Services / Biens">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value=""></option>
+            </select>
+            <label for="donsPretsServicesBiens">Dons / Prêts / Services / Biens</label>
+        </div>
+        <div class="form-floating mb-3">
+            <select class="form-select" id="toutesLesCategories" aria-label="Toutes les catégories">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select>
+            <label for="toutesLesCategories">Toutes les catégories</label>
+        </div>
+        <div class="form-floating mb-3">
+            <select class="form-select" id="touteLaFrance" aria-label="Toute la France">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select>
+            <label for="touteLaFrance">Toute la France</label>
+        </div>
+        <div class="form-floating mb-3">
+            <select class="form-select" id="tousLesDepartements" aria-label="Tous les départements">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select>
+            <label for="tousLesDepartements">Tous les départements</label>
+        </div>
+        <div class="d-none" id="submitSuccessMessage">
+            <div class="text-center mb-3">
+                <div class="fw-bolder">Form submission successful!</div>
+                <p>To activate this form, sign up at</p>
+                <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
+            </div>
+        </div>
+        <div class="d-none" id="submitErrorMessage">
+            <div class="text-center text-danger mb-3">Error sending message!</div>
+        </div>
+        <div class="d-grid">
+            <button class="btn btn-primary btn-lg disabled" id="submitButton" type="submit">Submit</button>
+        </div>
+    </form>
+</div>
+<script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
 
-        </header>
-        <!-- Section-->
 
         <?php require_once 'inc/footer.php'; ?>
         <!-- Bootstrap core JS-->
