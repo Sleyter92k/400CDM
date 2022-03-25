257ecbf1b82ff919d30c97f915a20630111a6f4e
LOW-GEEK <vincent.gladone@colombbus.org>
2022-03-25 12:52:37 +0100

    footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,21 @@
 <!-- Footer-->
-<footer class="py-5">
-<i class="bi bi-instagram"></i>
-<i class="bi bi-facebook"></i>
+<footer class="py-5 text-color=white">
+    <div class="container">
+        
+        <ul class=" text-color=white">
+            <li>Qui somme nous?</li>
+            <li>CGU</li>
+            <li>CGV</li>
+            <li>Mentions légales</li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        <div>
+            <i class="bi bi-instagram"></i>
+            <i class="bi bi-facebook"></i>
+        </div>
+    </div>
     
     <div class="container"><p class="m-0 text-white">Copyright &copy; 400 Coups de Main 2022</p></div>
         
