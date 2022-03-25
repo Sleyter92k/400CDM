104e21f78aed29bdc8a5c77ebb95577f55653091
Sleyter Monteiro <sleyter.monteiro@colombbus.org>
2022-03-25 11:12:15 +0100

    css

---
+++ inc/footer.php
@@ -0,0 +1,8 @@
+<!-- Footer-->
+<footer class="py-5">
+<i class="bi bi-instagram"></i>
+<i class="bi bi-facebook"></i>
+    
+    <div class="container"><p class="m-0 text-white">Copyright &copy; 400 Coups de Main 2022</p></div>
+        
+</footer>
---
+++ inc/navbar.php
@@ -0,0 +1,25 @@
+
+<nav id="navbar" class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#">Logo</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Accueil</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Recherche</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Déposer une annonce</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
+                    </ul>
+                        
+                        <button class="btn btn-outline-dark" type="submit">
+                            Connexion
+                        </button>
+
+                        <button class="btn btn-outline-dark" type="submit">
+                            Inscription
+                        </button>
+
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
