aed453f0ca8d36349aeee0735426c73a59f121c3
Sleyter Monteiro <sleyter.monteiro@colombbus.org>
2022-04-07 12:30:59 +0200

    sql

--- docs/400cdm.sql
+++ docs/400cdm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : dim. 03 avr. 2022 à 13:16
+-- Généré le : jeu. 07 avr. 2022 à 12:30
 -- Version du serveur : 10.4.22-MariaDB
 -- Version de PHP : 7.4.27
 
@@ -47,10 +47,8 @@ CREATE TABLE `annonces` (
 --
 
 INSERT INTO `annonces` (`id_annonce`, `type_annonce`, `type_de_cdm`, `titre`, `description`, `code_postal`, `ville`, `adresse`, `photo`, `categorie`) VALUES
-(1, 'Dons', 'Offre', 'Ecrans de bureau', 'vga', 97100, 'pap', '1 rue machin', '', ''),
-(2, 'Biens', 'Offre', 'Ecrans de bureau', 'hdmi', 97100, 'pap', '1 rue machin', '', ''),
-(3, 'Biens', 'Offre', 'Ecrans de bureau', 'hdmi', 97100, 'pap', '1 rue machin', '', ''),
-(4, 'Biens', 'Offre', 'Ecrans de bureau', 'hdmi', 97100, 'pap', '1 rue machin', '', '');
+(34, 'Biens', 'Demande', 'ordinateur', 'odrinateur', 75001, 'paris', 'rue du soleil', 'photos/toto.jpg', 'Coup de main'),
+(35, 'Services', 'Demande', 'Patrick', 'Test', 92600, 'Asnieres', '10 Rue Picquart', 'photos/patrick2.png', 'Bricolage');
 
 -- --------------------------------------------------------
 
@@ -72,7 +70,11 @@ CREATE TABLE `contact` (
 --
 
 INSERT INTO `contact` (`id_contact`, `objet`, `nom`, `prenom`, `mail`, `message`) VALUES
-(1, 'Test Objet', 'Monteiro', 'Sleyter', 'sleytermonteiro92@protonmail.com', 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC');
+(1, 'Test Objet', 'Monteiro', 'Sleyter', 'sleytermonteiro92@protonmail.com', 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC'),
+(2, '5', '', '', 'fgfggggggggggggggg', 'fgggggggggggggggggg'),
+(3, '4', '', '', 'sleytermonteiro92@protonmail.com', 'Coucou test'),
+(4, '4', '', '', 'sleytermonteiro92@protonmail.com', 'TEST'),
+(5, '5', '', '', 'sleyter@gmail.com', 'TEST MSG');
 
 -- --------------------------------------------------------
 
@@ -90,20 +92,18 @@ CREATE TABLE `membres` (
   `adresse` varchar(50) NOT NULL,
   `code_postal` int(5) NOT NULL,
   `ville` varchar(50) NOT NULL,
+  `mobile` int(10) NOT NULL,
   `mdp` varchar(255) NOT NULL,
-  `statut` int(1) NOT NULL DEFAULT 0
+  `statut` int(1) NOT NULL DEFAULT 0 COMMENT '0 = client\r\n1 = admin'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `membres`
 --
 
-INSERT INTO `membres` (`id_membre`, `prenom`, `nom`, `civilite`, `pseudo`, `mail`, `adresse`, `code_postal`, `ville`, `mdp`, `statut`) VALUES
-(2, 'linz', 'zeeze', 'Mme', 'ratpi', 'sleyter@gmail.com', 'azazaz', 92600, 'Asnières-sur-Seine', '$2y$10$f/HKt891OhI4JOUxtWxHEeq4oBPVHfci30oqsh8TBEgwUvXWJqtLW', 0),
-(3, 'Lina', 'Moundir', 'Mme', 'linda', 'linda@moundir.com', '10 Rue Picquart', 92600, 'Asnières-sur-Seine', '$2y$10$/nHN6A/oOZtEaNiFqXNYnuhKZ9cBKLDY7Up17GPwfBAD/BxsX62Wu', 0),
-(4, 'Sleyter', 'Monteiro', 'Mr', 'sleyter92k', 'sleytermonteiro92@protonmail.com', '10 Rue Picquart', 92600, 'Asnières-sur-Seine', '$2y$10$8P1cH9X9rVQLyLOdBJIOUeOR5jBCw/1j.LErEFHk.lFm5KZNCs2Vy', 0),
-(5, 'arno', 'pierre', 'Mr', 'Nous', 'g@mail.com', '1233', 97100, 'pap', '$2y$10$xNzcGM5DYkt1nJKjibHxgOrLzj5TXoc1c5ZXW2pumfoYFRFftZHe.', 0),
-(6, 'Vincenzo', 'GLAD', 'Mr', 'LAW-GEEK', 'glad@gmail.com', 'La Marina', 97100, 'pap', '$2y$10$m4Bs3P7Ts3WC8qwj1QEI.edWkJpo2W/egFQfNyC19eFffIYzsR8aW', 1);
+INSERT INTO `membres` (`id_membre`, `prenom`, `nom`, `civilite`, `pseudo`, `mail`, `adresse`, `code_postal`, `ville`, `mobile`, `mdp`, `statut`) VALUES
+(8, 'Sleyter', 'Monteiro', 'Mr', 'sleyter92k', 'sleytermonteiro92@protonmail.com', '10 Rue Picquart', 92600, 'Asnières-sur-Seine', 699746092, '$2y$10$IyZx6Wy1WkXarJTDz5w6GO1dodzlrFuMoIq9SCYXQfUcFjlQQgMQy', 1),
+(10, 'Julie', 'Murie', 'Mme', 'julia', 'julie@gmail.com', 'rue de normandie', 92500, 'Normandie', 789854789, '$2y$10$SCqtz7zzLXY0Rwa4mPdGWeNJQGtkzZaZWhOXGc0P/iUPPfEsDAMpq', 0);
 
 --
 -- Index pour les tables déchargées
@@ -135,19 +135,19 @@ ALTER TABLE `membres`
 -- AUTO_INCREMENT pour la table `annonces`
 --
 ALTER TABLE `annonces`
-  MODIFY `id_annonce` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_annonce` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT pour la table `contact`
 --
 ALTER TABLE `contact`
-  MODIFY `id_contact` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_contact` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pour la table `membres`
 --
 ALTER TABLE `membres`
-  MODIFY `id_membre` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_membre` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
