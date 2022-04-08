81b1b0c4ff271c453f1dc26e5eff981ec919a0e4
Sleyter Monteiro <sleyter.monteiro@colombbus.org>
2022-04-08 12:25:21 +0200

    navbar

--- inc/navbar.php
+++ inc/navbar.php
@@ -16,14 +16,23 @@
                     </ul>
 
                         <div class="col-md-2 col-sm-2">
-                      <?php  if (estConnecte()) {
-                    echo '<li class="nav-item"><a class="btn btn-secondary" href="' .RACINE_SITE. 'connexion.php?action=deconnexion">Se déconnecter</a></li>';
-                  } ?>
-                            <div class="row">
-                        <button  class="btn btn-outline-info" ><a href="connexion.php">Connexion</a></button>
-                        <button  class="btn btn-outline-info mt-2" ><a href="inscription.php">Inscription</a></button>
-                            </div>
-                        </div>
+                        <?php if(!EstConnecte()) { ?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="connexion.php">Connexion</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="inscription.php">Inscription</a>
+                            </li>
+                        <?php } else { ?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="profil.php">Profil</a>
+                            </li>
+
+                            <li class="nav-item">
+                           <?php echo '<a class="btn btn-danger" href="' .RACINE_SITE. 'connexion.php?action=deconnexion">Se déconnecter</a>'; ?>
+                            </li>
+         
+                        <?php } ?>
                     </div>
                 </div>
             </div>
