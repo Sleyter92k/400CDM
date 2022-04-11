6e2444abc850d1ff0db451c80edd5f41b4a3d1c1
Sleyter MONTEIRO <87656234+Sleyter92k@users.noreply.github.com>
2022-04-11 10:38:01 +0200

    footer

---
+++ mention.php
@@ -0,0 +1,42 @@
+<?php require_once 'inc/fonction.php';  // appel du fichier de fonction 
+      require_once 'inc/log_bdd.php';
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      <meta name="description" content="" />
+      <meta name="author" content="" />
+      <title>400 Coups de Main</title>
+      <script src="https://kit.fontawesome.com/5ba36090d7.js" crossorigin="anonymous"></script>
+      <!-- Favicon-->
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;600&display=swap" rel="stylesheet">  
+      <!-- Bootstrap icons-->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+      <!-- Core theme CSS (includes Bootstrap)-->
+      <link href="css/bootstrap.css" rel="stylesheet" />
+      <link href="css/style.css" rel="stylesheet" />
+  </head>
+  <body>
+      <?php require_once 'inc/navbar.php'; ?> 
+      <section class="py-5">
+        <div class="container">
+          <section class="row justify-content-center">
+            <div class="col-md-6 m-2">
+              <h1>Mentions légales</h1><div><br></div><div><strong>Introduction</strong></div><div>Devant le développement des nouveaux outils de communication, il est nécessaire de porter une attention particulière à la protection de la vie privée. C’est pourquoi, nous nous engageons à respecter la confidentialité des renseignements personnels que nous collectons.</div><div><br></div><h2>Collecte des renseignements personnels</h2><div><br></div><div><ul><li>Prénom</li><li>Adresse postale</li><li>Code postal</li><li>Adresse électronique</li><li>Numéro de téléphone / télécopieur</li></ul></div><div>Les renseignements personnels que nous collectons sont recueillis au travers de formulaires et grâce à l’interactivité établie entre vous et notre site Web. Nous utilisons également, comme indiqué dans la section suivante, des fichiers témoins et/ou journaux pour réunir des informations vous concernant.</div><div><br></div><h2>Formulaires&nbsp;et interactivité:</h2><div>Vos renseignements personnels sont collectés par le biais de formulaire, à savoir :</div><div><br></div><div><ul><li>Formulaire d'inscription au site Web</li><li>Formulaire de commande</li></ul></div><div>Nous utilisons les renseignements ainsi collectés pour les finalités suivantes :</div><div><br></div><div><ul><li>Suivi de la commande</li><li>Informations / Offres promotionnelles</li><li>Statistiques</li><li>Contact</li></ul></div><div>Vos renseignements sont également collectés par le biais de l’interactivité pouvant s’établir entre vous et notre site Web et ce, de la façon suivante:</div><div><br></div><div><ul><li>Commentaires </li><li>Correspondance</li></ul></div><div>Nous utilisons les renseignements ainsi collectés pour les finalités suivantes :</div><div><br></div><div><ul><li>Informations ou pour des offres promotionnelles</li><li>Statistiques</li><li>Contact </li></ul></div><div><br></div><h2>Droit d’opposition et de retrait</h2><div>Nous nous engageons à vous offrir un droit d’opposition et de retrait quant à vos renseignements personnels.</div><div>Le droit d’opposition s’entend comme étant la possiblité offerte aux internautes de refuser que leurs renseignements personnels soient utilisées à certaines fins mentionnées lors de la collecte.</div><div><br></div><div>Le droit de retrait s’entend comme étant la possiblité offerte aux internautes de demander à ce que leurs renseignements personnels ne figurent plus, par exemple, dans une liste de diffusion.</div><div><br></div><div><strong>Pour pouvoir exercer ces droits, vous pouvez :</strong></div><div><br></div><div></div><div></div><div></div><div></div><div><br></div><h2>Droit d’accès</h2><div>Nous nous engageons à reconnaître un droit d’accès et de rectification aux personnes concernées désireuses de consulter, modifier, voire radier les informations les concernant.</div><div><br></div><div><strong>L’exercice de ce droit se fera :</strong></div><div><br></div><div></div><div></div><div></div><div></div><div><br></div><h2>Sécurité</h2><div>Les renseignements personnels que nous collectons sont conservés dans un environnement sécurisé. Les personnes travaillant pour nous sont tenues de respecter la confidentialité de vos informations.</div><div><br></div><div>Pour assurer la sécurité de vos renseignements personnels, nous avons recours aux mesures suivantes :</div><div><br></div><div><ul><li>Protocole SSL</li><li>Protocole SET</li><li>Gestion des accès - personne autorisée</li><li>Gestion des accès - personne concernée</li><li>Logiciel de surveillance du réseau</li><li>Sauvegarde informatique</li><li>Identifiant / mot de passe</li><li>Pare-feu</li></ul></div><div>Nous nous engageons à maintenir un haut degré de confidentialité en intégrant les dernières innovations technologiques permettant d’assurer la confidentialité de vos transactions. Toutefois, comme aucun mécanisme n’offre une sécurité maximale, une part de risque est toujours présente lorsque l’on utilise Internet pour transmettre des renseignements personnels.</div><div><br></div><h2>Législation</h2><div>Nous nous engageons à respecter les dispositions législatives énoncées dans :</div><div><br></div><div>Législation:  données électroniques</div>
+            </div>
+          </section>
+                
+        </div> <!-- fin container  -->
+        <!-- footer en require  -->
+        <?php require_once 'inc/footer.php'; ?>
+      </section>
+      <!-- Optional JavaScript; choose one of the two! -->
+      <!-- Option 1: Bootstrap Bundle with Popper -->
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
