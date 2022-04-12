ac5057458b0c65ff69d6a6ba3394358e90b6b053
Sleyter MONTEIRO <87656234+Sleyter92k@users.noreply.github.com>
2022-04-12 23:20:39 +0200

    correction navbar profil

--- profil.php
+++ profil.php
@@ -140,8 +140,11 @@ if (isset($_GET['action']) && $_GET['action'] == 'supprimer' && isset($_GET['id_
         </div>
       </div>
 
-    <script type="text/javascript"></script>
+              
+    
 
     <?php require_once 'inc/footer.php'; ?>
+    <script type="text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
 </html>
